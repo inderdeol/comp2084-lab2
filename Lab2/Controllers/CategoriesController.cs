01fce335d6d8396118d4695bda2a6e30ee261cc5
Inderdeol <iaminderdeol123@gmail.com>
2019-09-20 16:04:35 -0400

    Lab2

---
+++ Lab2/Controllers/CategoriesController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab2.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        // GET: Categories
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Details(String parameter)
+        {
+            ViewBag.Message = "You Selected Category is: " + parameter;
+            return View();
+        }
+
+        public ActionResult Food()
+        {
+           
+            return View();
+        }
+
+        public ActionResult Sports()
+        {
+            
+            return View();
+        }
+
+        public ActionResult Tech()
+        {
+            
+            return View();
+        }
+    }
+}
